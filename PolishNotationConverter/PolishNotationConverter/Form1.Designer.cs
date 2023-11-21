886de2e0b55909354aa306dfa9a6b8b8d76e76e2
sandi-andres <98196556+sandi-andres@users.noreply.github.com>
2023-11-20 20:44:35 -0600

    Add files via upload

---
+++ PolishNotationConverter/PolishNotationConverter/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PolishNotationConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInfix = new System.Windows.Forms.TextBox();
+            this.labelPostfix = new System.Windows.Forms.Label();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.labelInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInfix
+            // 
+            this.textBoxInfix.Location = new System.Drawing.Point(36, 66);
+            this.textBoxInfix.Name = "textBoxInfix";
+            this.textBoxInfix.Size = new System.Drawing.Size(167, 20);
+            this.textBoxInfix.TabIndex = 0;
+            // 
+            // labelPostfix
+            // 
+            this.labelPostfix.AutoSize = true;
+            this.labelPostfix.Location = new System.Drawing.Point(33, 99);
+            this.labelPostfix.Name = "labelPostfix";
+            this.labelPostfix.Size = new System.Drawing.Size(0, 13);
+            this.labelPostfix.TabIndex = 1;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(227, 66);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(98, 22);
+            this.buttonConvert.TabIndex = 2;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelInstructions
+            // 
+            this.labelInstructions.AutoSize = true;
+            this.labelInstructions.Location = new System.Drawing.Point(22, 19);
+            this.labelInstructions.Name = "labelInstructions";
+            this.labelInstructions.Size = new System.Drawing.Size(321, 26);
+            this.labelInstructions.TabIndex = 3;
+            this.labelInstructions.Text = "Insert a prefix expression. I can only contain 1 digit whole numbers.\r\nAccepted o" +
+    "perators: + – * / ^ %";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 144);
+            this.Controls.Add(this.labelInstructions);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.labelPostfix);
+            this.Controls.Add(this.textBoxInfix);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInfix;
+        private System.Windows.Forms.Label labelPostfix;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Label labelInstructions;
+    }
+}
+
---
+++ PolishNotationConverter/PolishNotationConverter/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PolishNotationConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!ValidateCharacters().Item1)
+                MessageBox.Show("You inserted " + ValidateCharacters().Item2 + " but only the following characters are permitted: 0123456789+–*/^%.");
+            else if (!ValidateDigits().Item1)
+                MessageBox.Show("You inserted " + ValidateDigits().Item2 + " but only 1 digit numers are permitted.");
+            else 
+            {
+                Converter converter = new Converter(textBoxInfix.Text);
+                labelPostfix.Text = converter.ConvertPostfix();
+            }
+        }
+
+        private (bool, string) ValidateDigits()
+        {
+            bool result = true;
+
+            string value = textBoxInfix.Text;
+
+            string numerError = "";
+
+            List<string> digits = value.Split('+', '(', ')', '–', '*', '/', '^', '%').ToList();
+
+            for (int i = 0; i < digits.Count; i++)
+                if (digits[i].Count() > 1)
+                {
+                    result = false;
+                    numerError = digits[i].ToString();
+                }
+            return (result, numerError);
+        }
+
+        private (bool, string) ValidateCharacters()
+        {
+            bool result = true;
+
+            string permitted = "0123456789+–*/^%()";
+
+            string digitError = "";
+
+            foreach (char c in textBoxInfix.Text)
+                if (!permitted.Contains(c))
+                {
+                    result = false;
+                    digitError = c.ToString();
+
+                    if (digitError == " ")
+                        digitError = "a space";
+                }
+            return (result, digitError);
+
+        }
+    }
+}
